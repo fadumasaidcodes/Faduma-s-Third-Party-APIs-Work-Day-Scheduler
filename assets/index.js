4de0faa7ed9cced04e4d90e9ab5b48b4af531081
fadumasaidibrahim <fadumasaid2022@gmail.com>
2023-01-30 19:51:09 +0000

    changed the issue with calender width

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var calendar = $(".container");
   var currentHour = moment().format("H");
 
-  for (var i = 9; i < 18; i++) {
-    var hourRow = $("<div>").addClass("row");
+  for (var i = 9; i < 20; i++) {
+    var hourRow = $("<div>").addClass("row input-group");
     var hourColumn = $("<div>").text(i + ":00").addClass("hour");
-    var eventColumn = $("<div>").addClass("event");
-    var eventInput = $("<textarea>").attr("class", "col-10");
+    // var eventColumn = $("<form>").addClass("event");
+    var eventInput = $("<textarea>").attr("class", "textarea form-control");
     var saveButton = $("<button>").html(`<i class="far fa-save"></i>`).addClass("saveBtn");
 
     if (i < currentHour) {
@@ -18,8 +18,10 @@ $(document).ready(function() {
     }
 
 
-    eventColumn.append(eventInput);
-    hourRow.append(hourColumn, eventColumn, saveButton);
+
+    // eventColumn.append(eventInput)
+    hourRow.append(hourColumn, eventInput, saveButton);
+
     calendar.append(hourRow);
   }
 });
