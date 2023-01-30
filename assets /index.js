5057c13c42f9ea79484b711b178efd682f47dc49
fadumasaidcodes <fadumasaid2022@gmail.com>
2023-01-30 16:01:39 +0000

    added HTMl

--- assets /index.js
+++ assets /index.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
-    var calendar = $(".container");
-    var currentHour = moment().format("H");
-  
-    for (var i = 0; i < 24; i++) {
-      var hourRow = $("<div>").addClass("row" );
-      var hourColumn = $("<div>").text(i + ":00");
-      var eventColumn = $("<div>").addClass("event");
-      var eventInput = $("<textarea>").attr("class", "col-10");
-      var saveButton = $("<button>").text("Save").addClass("saveBtn");
-  
-      eventColumn.append(eventInput);
-      hourRow.append(hourColumn, eventColumn, saveButton);
-      calendar.append(hourRow);
+  var calendar = $(".container");
+  var currentHour = moment().format("H");
+
+  for (var i = 9; i < 18; i++) {
+    var hourRow = $("<div>").addClass("row");
+    var hourColumn = $("<div>").text(i + ":00").addClass("hour");
+    var eventColumn = $("<div>").addClass("event");
+    var eventInput = $("<textarea>").attr("class", "col-10");
+    var saveButton = $("<button>").html(`<i class="far fa-save"></i>`).addClass("saveBtn");
+
+    if (i < currentHour) {
+      eventInput.addClass("past");
+    } else if (i === parseInt(currentHour)) {
+      eventInput.addClass("present");
+    } else {
+      eventInput.addClass("future");
     }
-  });
-  
+
+
+    eventColumn.append(eventInput);
+    hourRow.append(hourColumn, eventColumn, saveButton);
+    calendar.append(hourRow);
+  }
+});
